cb65bbf003f45b4e356bec8831d8d5dd770e9ea1
Kimberly Yeo <102574539+yeoki-osu@users.noreply.github.com>
2025-05-06 18:36:02 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://adulbrich.github.io",
+  site: "https://yeoki-osu.github.io/",
   base: "/demo-github-actions",
 });
